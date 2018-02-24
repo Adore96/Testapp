0cd2d5b9a3e1740ad19d8f755ac931ddfd4d79a3
Adore96 <kasunadore@icloud.com>
2018-02-24 16:34:27 +0530

    change in one file

--- resources/views/newpage.blade.php
+++ resources/views/newpage.blade.php
@@ -3,6 +3,6 @@
 @section('content')
   <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
   <a href="{{url('/home')}}">
-    <img src="{{asset('one.jpg')}}" alt="" height="100px" width="100px">
+    <img src="{{asset('one.jpg')}}" alt="" height="100px" width="100px">sjnfjdsnfjnsdfjndjnf
   </a>
 @endsection
